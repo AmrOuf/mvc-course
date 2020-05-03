bb1933cd1e6ceedbff6e6fa09f729bf97813bd81
Amr <amr.ouf96@gmail.com>
2020-05-03 15:30:38 +0200

    Lab 01

---
+++ Day1/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public class User
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+
+        }
+
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Form(User user)
+        {
+            if (user != null && user.Name != null)
+            {
+                ViewBag.Name = user.Name;
+                return View("Welcome");
+            }
+            return View();
+        }
+
+    }
+}
