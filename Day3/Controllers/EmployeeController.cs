a5575e06497ed02a0582447330c8ef63c28f4232
Amr <amr.ouf96@gmail.com>
2020-05-06 08:23:32 +0200

    Lab 03

---
+++ Day3/Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using Day1.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+
+
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View(context.Employees.ToList());
+        }
+
+        [HttpGet]
+        public ViewResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Employee employee)
+        {
+            if(ModelState.IsValid)
+            {
+                context.Employees.Add(employee);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ Day3/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Day1
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(128)]
+        [EmailAddress]
+        public string Email { get; set; }
+        public int Salary { get; set; }
+        [MaxLength(256)]
+        public string Address { get; set; }
+    }
+}
